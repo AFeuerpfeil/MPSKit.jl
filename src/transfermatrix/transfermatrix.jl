b5391a01fe864752cfa21adc3bdd29c63ebb4948
AFeuerpfeil <andreas.feuerpfeil@gmail.com>
2026-09-05 19:00:47 -0700

    fix

--- src/transfermatrix/transfermatrix.jl
+++ src/transfermatrix/transfermatrix.jl
@@ -67,7 +67,7 @@ end
 function TransferMatrix(a::AbstractVector, b, c::AbstractVector, isflipped = false;
         backend::AbstractBackend = DefaultBackend(), allocator = DefaultAllocator()
     )
-    tot = ProductTransferMatrix(convert(Vector, TransferMatrix.(a, b, c, false, Ref(backend), Ref(allocator))))
+    tot = ProductTransferMatrix(convert(Vector, map(x-> TransferMatrix(x, b, c, isflipped; backend = backend, allocator = allocator), zip(a, b, c))))
     return isflipped ? flip(tot) : tot
 end
 
